28f6c841303ff2172263301b9998a194ebafb0f9
Youngjin Kim <youngj0908@gmail.com>
2017-04-21 20:47:07 +0900

    inception score : mnist

--- inception_score/eval_mnist.py
+++ inception_score/eval_mnist.py
@@ -1,33 +1,38 @@
-import model_cifar10 as model
+import model_mnist as model
 import numpy as np
 import os
 from IPython import embed
 
 exps = [
-    'samples/EXP40/cifar10/noise/1491279930',
+    'ICCV2017/samples/EXP38/dcgan/mnist/noise/1490076837',
 ]
 maxs = []
 
 for exp in exps:
-    base_dir = '/data/whyjay/NIPS2017/'
+    #base_dir = '/data/whyjay/NIPS2017/'
+    base_dir = '/data/whyjay/'
+
     sample_dir = base_dir + exp
-    samples = sorted(list(set([int(s.split('_')[-1][:-4]) for s in os.listdir(sample_dir) if 'samples' in s])))
+    samples = sorted(list(set([int(s.split('_')[-1][:-4]) for s in os.listdir(sample_dir) if 'samples' in s])))[:100]
     print "num samples : %d" % (len(samples))
     print sample_dir
 
     mean_stddev = np.zeros((len(samples),2))
+    preds = []
     for i, s in enumerate(samples):
         #with open(sample_dir + '/samples_rec_%d.npy'%s) as f:
         with open(sample_dir + '/samples_%d.npy'%s) as f:
             images = np.load(f)
         images = np.split(images, images.shape[0])
-        images = [(im.reshape(im.shape[1:]) + 1)*255./2 for im in images]
+        images = [im.reshape(im.shape[1:]) for im in images]
 
-        mean, stddev = model.get_inception_score(images)
-        print "---------- SCORE in %s ------------" % s
-        print "%f, %f"%(mean, stddev)
+        mean, stddev, pred = model.get_inception_score(images)
+        if i % 10 == 0:
+            print "---------- SCORE in %s ------------" % s
+            print "%f, %f"%(mean, stddev)
         mean_stddev[i,0] = mean
         mean_stddev[i,1] = stddev
+        preds.append(pred)
 
     max_idx = mean_stddev[:,0].argmax()
     maxs.append((exp, mean_stddev[max_idx,0], mean_stddev[max_idx,1], samples[max_idx]))
@@ -35,4 +40,6 @@ for exp in exps:
 
     with open(sample_dir+'/scores.npy', 'w') as f:
         np.save(f, mean_stddev)
+    with open(sample_dir+'/predictions.npy', 'w') as f:
+        np.save(f, preds)
 print maxs
---
+++ inception_score/model_mnist.py
@@ -0,0 +1,89 @@
+# Code derived from tensorflow/tensorflow/models/image/imagenet/classify_image.py
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os.path
+import sys
+import tarfile
+
+import sys
+sys.path.insert(0, '/data/whyjay/NIPS2017')
+import numpy as np
+from six.moves import urllib
+import tensorflow as tf
+import glob
+import scipy.misc
+import math
+import sys
+from ops import *
+
+MODEL_DIR = '/data/whyjay/NIPS2017/checkpoint/mnist_evaluator/'
+softmax = None
+x = None
+
+# Call this function with list of images. Each of elements should be a
+# numpy array with values ranging from 0 to 255.
+def get_inception_score(images, splits=10):
+    assert(type(images) == list)
+    assert(type(images[0]) == np.ndarray)
+    assert(len(images[0].shape) == 3)
+    assert(np.min(images[0]) >= 0.0)
+    inps = []
+
+    for img in images:
+        img = img.astype(np.float32)
+        inps.append(np.expand_dims(img, 0))
+
+    bs = 100
+    with tf.Session() as sess:
+        ckpt = tf.train.get_checkpoint_state(MODEL_DIR)
+        if ckpt and ckpt.model_checkpoint_path:
+            ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
+        saver = tf.train.Saver()
+        saver.restore(sess, os.path.join(MODEL_DIR, ckpt_name))
+
+        preds = []
+        n_batches = int(math.ceil(float(len(inps)) / float(bs)))
+
+        for i in range(n_batches):
+            sys.stdout.write(".")
+            sys.stdout.flush()
+            inp = inps[(i * bs):min((i + 1) * bs, len(inps))]
+            inp = np.concatenate(inp, 0)
+            pred = sess.run(softmax, {x: inp})
+            preds.append(pred)
+
+        preds = np.concatenate(preds, 0)
+        scores = []
+
+    for i in range(splits):
+        part = preds[(i * preds.shape[0] // splits):((i + 1) * preds.shape[0] // splits), :]
+        kl = part * (np.log(part) - np.log(np.expand_dims(np.mean(part, 0), 0)))
+        kl = np.mean(np.sum(kl, 1))
+        scores.append(np.exp(kl))
+
+    return np.mean(scores), np.std(scores), preds
+
+# This function is called automatically.
+def _init_model():
+    global softmax
+    global x
+    if not os.path.exists(MODEL_DIR):
+        os.makedirs(MODEL_DIR)
+
+    # Works with an arbitrary minibatch size.
+    x = tf.placeholder(tf.float32, shape=[None, 28, 28, 1])
+
+    h = tf.nn.relu(conv2d(x, 32, d_h=1, d_w=1, name='h0'))
+    h = tf.nn.max_pool(h, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    h = tf.nn.relu(conv2d(h, 64, d_h=1, d_w=1, name='h1'))
+    h = tf.nn.max_pool(h, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    h = tf.reshape(h, [-1, 7*7*64])
+    h = linear(h, 1024, scope='h3')
+    h = tf.nn.dropout(h, 1.0)
+    logits = linear(h, 10, scope='logit')
+    softmax = tf.nn.softmax(logits)
+
+if softmax is None or x is None:
+    _init_model()
